e073a39ef7c5afd806182455d0a61ac202137399
Formacion <Formacion@bomberosfloridablanca.com>
2024-08-13 14:24:28 -0500

    Comienza el ejercicio de Entity framework

---
+++ Modelos/Categorias.cs
@@ -0,0 +1,8 @@
+namespace ProjectoEF.Models;
+
+public class Categorias
+{
+    public Guid CategoriaID {get; set;}
+    public string Nombre {get; set;}
+    public string Descripcion {get;set;}    
+}
