66c7d1ce00905cc97ca3cee174d64e3e281201e3
Formacion <Formacion@bomberosfloridablanca.com>
2024-08-22 13:36:18 -0500

    Se realizo una actualizacion en la base de datos

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
 {
     var DatosTareas=dbContext.Tareas;
     //return Results.Ok(DatosTareas.Include(X=> X.Categoria).Where(x=> x.PrioridadTarea == ProjectoEF.Modelos.Prioridad.Media));
-    return Results.Ok(DatosTareas.Include(X=> X.Categoria));
+    return Results.Ok(DatosTareas);
 });
 app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
 {
@@ -32,6 +32,21 @@ app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromB
     //await dbContext.Tareas.AddAsync(tarea); Esta es otra forma de hacerlo y esta bien tambien
     await dbContext.SaveChangesAsync();
     return Results.Ok();
+});
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea,[FromRoute] Guid id) =>
+{
+    var TareaActual= dbContext.Tareas.Find(id);
+    if (TareaActual!=null)
+    {
+        TareaActual.CategoriaId=tarea.CategoriaId;
+        TareaActual.Titulo=tarea.Titulo;
+        TareaActual.PrioridadTarea=tarea.PrioridadTarea;
+        TareaActual.Descripcion=tarea.Descripcion;
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+    return Results.NotFound();
 });
 
 app.Run();  
