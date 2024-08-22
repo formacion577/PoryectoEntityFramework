57d6d5de75a1bc01cc801805b11d25c4067ef5b9
Andres Parra Acevdo <parrace2@hotmail.com>
2024-08-21 20:23:32 -0500

    Se corrio unas lineas de Codigo y serealizo el codigo para guardar datos en entity framework

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using System.Data;
 using Microsoft.AspNetCore.Mvc;  
-using Microsoft.EntityFrameworkCore;  
+using Microsoft.EntityFrameworkCore;
+using ProjectoEF.Modelos;
 using proyetoef;  
 
 var builder = WebApplication.CreateBuilder(args);  
@@ -17,11 +19,19 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
     await dbContext.Database.MigrateAsync();  
     return Results.Ok("Base de datos en SQL Server: " + !dbContext.Database.IsInMemory());  
 });
-app.MapGet("/api/tareas", async ([FromServices] TareasContext dbconexion) =>
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
 {
-    var DatosTareas=dbconexion.Tareas;
-    var DatosCategoria=dbconexion.Categorias;
+    var DatosTareas=dbContext.Tareas;
     return Results.Ok(DatosTareas.Include(x=> x.Categoria).Where(x=> x.PrioridadTarea== ProjectoEF.Modelos.Prioridad.Baja));
+});
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
+{
+    tarea.TareaID=Guid.NewGuid();
+    tarea.FechaCreacion=DateTime.Now;
+    await dbContext.AddAsync(tarea);
+    //await dbContext.Tareas.AddAsync(tarea); Esta es otra forma de hacerlo y esta bien tambien
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
 });
 
 app.Run();  
